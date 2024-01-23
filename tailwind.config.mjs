259af68bae49ba0f51baa6dc6490cdc47f11705f
eduardosasso <eduardo.sasso@gmail.com>
2024-01-22 22:01:47 -0800

    theme config, first gotham

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,38 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Avenir",
+          "Montserrat",
+          "Corbel",
+          "URW Gothic",
+          "source-sans-pro",
+          "sans-serif",
+        ],
+        // ... include other font families if needed
+      },
+      colors: {
+        // Light Theme Colors
+        bearGotham: {
+          primary: "#6aa9b9", // Dark text
+          secondary: "#2db98c", // Light grey text
+          background: "#10151c",
+          accent: "#e47b57", // Accent color similar to Bear's yellow
+          // Add other colors as needed
+        },
+        // Dark Theme Colors
+        bearDark: {
+          primary: "#FFFFFF", // White text
+          secondary: "#A4A4A4", // Light grey text
+          background: "#1C1B1B", // Dark background
+          accent: "#FFD580", // Accent color similar to Bear's yellow
+          // Add other colors as needed
+        },
+      },
+      // Add other theme customizations like fontFamily if needed
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
